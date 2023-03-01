5c0126f45dad7596d49b68c3e951231609b5d24f
mizonov <mizonov.iv@gmail.com>
2023-03-01 08:54:04 +0500

    no message

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar-wrapper">
+    <div>Логотип</div>
+    <ul>
+      <li>Все товары</li>
+      <li>Одежда</li>
+      <li>Аксессуары</li>
+      <li>Скидки</li>
+      <li>Подарочные сертификаты</li>
+    </ul>
+    <div>Адрес</div>
+    <div>О бренде</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar-wrapper {
+  display: flex;
+}
+</style>
